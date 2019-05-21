bda5dfd937c6645dbf20b5aeea50a82ec462dd76
Jwabin <46057721+Jwabin@users.noreply.github.com>
2019-05-21 20:40:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 function check(){
+        var question1 = document.quiz.question1.value;
+        var question2 = document.quiz.question2.value;
+        var question3 = document.quiz.question3.value;
+        var correct = 0;
+        
+        if (question1 == "Brussel") {
+        	correct++;
+}
+	if (question2 == "Amsterdam") {
+        	correct++;
+}
+	if (question3 == "Berlijn") {
+        	correct++;
+}
         document.getElementById("after_submit").style.visibility = "visible";
+	doumment.getElementById("number_correct").innerHTML = "Jij hebt" + correct + "het juist";
 }
